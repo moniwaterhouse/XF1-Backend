06b2ae981191a3cf0c5d12ad342004fc810ac5f2
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2022-05-04 00:10:43 -0600

    Task #51 - Crear request insertar competencias
    Se creó la configuración inicial del servidor y se añadió método para añadir competencia.

---
+++ server/XF1-Backend/Models/CompetenciasDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace XF1_Backend.Models
+{
+    public class CompetenciasDbContext : DbContext
+    {
+        public DbSet<Competencias> Competencias { get; set; }
+        public CompetenciasDbContext(DbContextOptions<CompetenciasDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Competencias>().ToTable("Competencias");
+        }
+    }
+}
