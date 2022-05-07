1358a73b471bb0c16640fab02faa6c9874153220
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2022-05-06 22:24:54 -0600

    Task #67 Unit testing Backend
    Added basic unit testing functions

---
+++ server/XF1-Backend-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XF1-Backend-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("XF1-Backend-Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("732377ea-09e9-469d-85a4-2f5335cead67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ server/XF1-Backend-Tests/TestsCampeonato.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using XF1_Backend.Controlles;
+
+
+namespace XF1_Backend_Tests
+{
+    [TestClass]
+    public class TestsCampeonato
+    {
+        [TestMethod]
+        public void InsertarValorNulo()
+        {
+            
+        }
+    }
+}
