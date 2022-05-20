9891c95b47c757fe3f9f86cc16f188cb9e283ab4
Ignacio Granados <ignaciogramar@gmail.com>
2022-05-20 15:31:51 -0600

    Task #91 y #92 Encriptar contraseñas

--- server/XF1-Backend/Logic/LogicFunctions.cs
+++ server/XF1-Backend/Logic/LogicFunctions.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using System.Security.Cryptography;
 using XF1_Backend.Models;
 
 namespace XF1_Backend.Logic
@@ -9,9 +10,9 @@ namespace XF1_Backend.Logic
     public class LogicFunctions
     {
         /*
-         * Descripcion: esta funcion carga un nuevo identificador para cualquier tabla que necesite
+         * Descripción: esta funcion carga un nuevo identificador para cualquier tabla que necesite
          * un identificador numerico entero basado en los id que ya existen.
-         * Input: carreras -> es una lista de las cerreras existentes.
+         * Input: carreras -> es una lista de las carreras existentes.
          * Output: newId -> un entero correspondiente al nuevo id.
          */
         public static int GenerarId(IEnumerable<Id> ids)
@@ -28,7 +29,7 @@ namespace XF1_Backend.Logic
         }
 
         /*
-         * Descripcióon: esta funcion genera una nueva llave de 6 caracteres alfanumericos
+         * Descripción: esta funcion genera una nueva llave de 6 caracteres alfanumericos
          * para diferenciar las carreras entre sí. La idea es que este no se repita con
          * llaves ya existentes.
          */
@@ -55,14 +56,14 @@ namespace XF1_Backend.Logic
         }
 
         /*
-         * Descripcion: Dada una lista de fechas ocupadas revisa que las fechas de inico y final no coincidan
+         * Descripción: Dada una lista de fechas ocupadas revisa que las fechas de inico y final no coincidan
          * en ninguno de los rangos posibles.
          * Entradas:
          * fechaInicio, fechaFinal -> fechas del rango que se desea añadir
          * fechas -> lista de las fechas actuales.
          * Salida: boleano que indica si hay un choque de fechas o no.
          */
-        public static bool RevisarFechas(DateTime fechaInicio, DateTime fechaFinal, IEnumerable<Fechas> fechas)
+        public static bool RevisarFechas(DateTime fechaInicio, DateTime fechaFin, IEnumerable<Fechas> fechas)
         {
 
             foreach (var fecha in fechas)
@@ -70,11 +71,11 @@ namespace XF1_Backend.Logic
                 if (DateTime.Compare(fechaInicio, fecha.FechaInicio) >= 0 &&
                     DateTime.Compare(fechaInicio, fecha.FechaFin) <= 0) return false;
 
-                if (DateTime.Compare(fechaFinal, fecha.FechaInicio) >= 0 &&
-                    DateTime.Compare(fechaFinal, fecha.FechaFin) <= 0) return false;
+                if (DateTime.Compare(fechaFin, fecha.FechaInicio) >= 0 &&
+                    DateTime.Compare(fechaFin, fecha.FechaFin) <= 0) return false;
 
                 if (DateTime.Compare(fechaInicio, fecha.FechaInicio) <= 0 &&
-                    DateTime.Compare(fechaFinal, fecha.FechaFin) >= 0) return false;
+                    DateTime.Compare(fechaFin, fecha.FechaFin) >= 0) return false;
             }
 
             return true;
@@ -82,12 +83,25 @@ namespace XF1_Backend.Logic
 
         /*
          * Descripcion: esta funcion toma un texto y lo encripta con MD5
-         * Entradas: contraseña sin encriptas
+         * Entradas: contraseña sin encriptar
          * Salida: contraseña encriptada
          */
         public static string EncriptarContrasena(string contrasena)
         {
-            return "Contraseña encriptada";
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(contrasena));
+
+            byte[] resultado = md5.Hash;
+
+            StringBuilder newContrasena = new StringBuilder();
+
+            for (int i = 0; i < resultado.Length; i++)
+            {
+                newContrasena.Append(resultado[i].ToString("x2"));
+            }
+
+            return newContrasena.ToString();
         }
     }
 }
